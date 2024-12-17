7aeb855de6d3a7eae0064289f958583382f11702
zippostorm <toghkklli@gmail.com>
2024-12-17 15:58:02 +0200

    fix framer-motion

--- frontend/src/components/FloatingShape.jsx
+++ frontend/src/components/FloatingShape.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion } from "motion/react";
+import { motion } from "framer-motion";
 
 const FloatingShape = ({ color, size, top, left, delay }) => {
   return (
